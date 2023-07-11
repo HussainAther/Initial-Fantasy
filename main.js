4f5b85aefc011a9c7627ceb25eb905f9937d2b90
Syed Hussain Ather <shussainather@gmail.com>
2023-07-11 18:40:44 -0400

    updated with buttons and options

--- main.js
+++ main.js
@@ -48,3 +48,41 @@ function handleNextDialog() {
   console.log("Next dialog button clicked!");
 }
 
+// Get references to the UI elements
+const playerNameInput = document.getElementById("player-name");
+const characterClassSelect = document.getElementById("character-class");
+const startGameButton = document.getElementById("start-game-button");
+
+// Function to start the game
+function startGame() {
+  // Get player name and character class selection
+  const playerName = playerNameInput.value;
+  const characterClass = characterClassSelect.value;
+
+  // Validate player name and character class
+  if (playerName === "") {
+    alert("Please enter your name.");
+    return;
+  }
+
+  if (characterClass === "") {
+    alert("Please select a character class.");
+    return;
+  }
+
+  // Start the game logic
+  console.log(`Starting game as ${playerName} - ${characterClass} class`);
+
+  // Opening Introduction
+  const introMessage = `Welcome, ${playerName}, to the land of Glagera! As a brave ${characterClass}, your journey begins here. Your mission is to travel to the legendary 7-11 and uncover the secrets hidden within. The path ahead will be filled with challenges and adventures. Are you ready to embark on this epic quest?`;
+  
+  // Display the introduction message to the player
+  showDialog(introMessage);
+
+  // Add your game logic here
+}
+
+// Automatically start the game when the page loads
+window.addEventListener("DOMContentLoaded", startGame);
+
+
